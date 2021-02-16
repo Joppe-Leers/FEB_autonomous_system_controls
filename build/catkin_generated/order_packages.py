2360f24e10924f83ba89a8769319ebf5cddaadca
Joppe-Leers <joppe.leers@hotmail.com>
2021-02-16 15:22:52 +0100

    created the catkin workspace

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/joppeleers/FEB_autonomous_system_controls/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/joppeleers/FEB_autonomous_system_controls/devel:$CMAKE_PREFIX_PATH"
+export PWD='/mnt/c/Users/joppe/Documents/FEB_autonomous_system_controls/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/joppeleers/FEB_autonomous_system_controls/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/joppeleers/FEB_autonomous_system_controls/src:$ROS_PACKAGE_PATH"
