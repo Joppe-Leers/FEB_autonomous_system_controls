2b7ec26fc6f2509b0bda6bf4ef20cb5c5f3dfbaa
Joppe-Leers <joppe.leers@hotmail.com>
2021-02-17 15:32:05 +0100

    added init method and code to read cones from the track

--- build/feb_as/catkin_generated/installspace/simWrap.py
+++ build/feb_as/catkin_generated/installspace/simWrap.py
@@ -5,20 +5,23 @@ from fs_msgs.msg import ControlCommand
 
 class SimWrap:
     def __init__(self):
-        print("constructor")
         ###### het is ook mogelijk om deze stappen in een andere functie te doen zodat het opbject op voorhand al aangemaakt kan worden. Ik weet niet wat het beste zou zijn. ######
+        print("simulator wrapper object created. call init to initialize")
 
+    def init(self):
+        print("#### start initialization process ####")
         # Get all the cones in the track by subscribing to topic /fsds/testing_only/track
-        rospy.init_node('trackListener', anonymous=True)
-        rospy.Subscriber("/fsds/testing_only/track", Track, callback)
-        rospy.spin()
+        print("subscribe to: /fsds/testing_only/track")
+        rospy.init_node('trackListener', anonymous=True, disable_signals=True)
+        rospy.Subscriber("/fsds/testing_only/track", Track, self.conesCallback)
+        rospy.spin() # wait here untill the node recieved all the cones
         
         # vorm de positie van de kegels om naar het formaat die gebruikt kan worden in de reward functie (twee lijnen -> blokken) Alec is met deze functie bezig
 
         # subscribe op topic odom om de positie en de orientatie van de auto te weten te komen
 
         # subscribe op topic extra info om in de reward score te weten te komen hoeveel kegels er geraakt zijn.
-        
+    
     # action must be in the format (steering, throttle, brake) : steering -1 to 1, throttle 0 to 1, brake 0 to 1
     def step(self, action):
         print("step")
@@ -46,17 +49,19 @@ class SimWrap:
         # als da is voeg die toe in een lijst
         # return conesInRange
 
-def callback(msg):
-    for cone in msg.track:
-        print("########")
-        print(cone.location.x)
-        print(cone.location.y)
-        print(cone.location.z)
+    def conesCallback(self, msg):
+        print("ROS message recieved")
+        self.cones = msg.track
+##        for cone in msg.track:
+##            print("########")
+##            print(cone.location.x)
+##            print(cone.location.y)
+##            print(cone.location.z)
+        print(len(self.cones), "cones saved in list")
+        rospy.signal_shutdown("don't need this node anymore")
         
 if __name__ == '__main__':
     simulationWrapper = SimWrap()
-    #simulationWrapper.getVision()
-    #while True:
-    #    print("nothing yet")
+    simulationWrapper.init()
     
         
